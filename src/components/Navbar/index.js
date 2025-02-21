a4fa49f21b38c286117c4b624b8318db865055ff
rafibachri <rafibachri05@gmail.com>
2025-02-21 15:29:07 +0700

    update chat ai version pop up

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,7 +13,7 @@ import Profile from "../../views/Profile";
 import { useState } from "react";
 import Swal from "sweetalert2";
 import { ArrowDown } from "react-bootstrap-icons";
-import { MdArrowDownward, MdKeyboardArrowDown } from "react-icons/md";
+import { MdArrowDownward, MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
 const Navbar = ({ user, logout, toogleMenu, showMenu }) => {
   const [showModal, setShowModal] = useState(false);
@@ -22,6 +22,18 @@ const Navbar = ({ user, logout, toogleMenu, showMenu }) => {
   const [isActive, setIsActive] = useState(false);
   const navigate = useNavigate();
 
+  const [tooltip, setTooltip] = useState("");
+  const [isOpen, setIsOpen] = useState(false);  // State untuk popup
+  const [selectedOption, setSelectedOption] = useState("1.5 Flash"); // Default teks navbar
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option); // Ubah teks navbar sesuai pilihan
+    setIsOpen(false); // Tutup popup setelah dipilih
+  };
   const toggleActive = () => {
     setIsActive(!isActive);
   };
@@ -45,7 +57,7 @@ const Navbar = ({ user, logout, toogleMenu, showMenu }) => {
     // }).then((result) => {
     //   if (result.isConfirmed) {
     //     logout({ email: user.email }).then(() => {
-          navigate("/login");
+    navigate("/login");
     //     });
     //   }
     // });
@@ -75,20 +87,51 @@ const Navbar = ({ user, logout, toogleMenu, showMenu }) => {
   return (
     <nav className={`navbar sticky-top ${isActive ? 'active' : ''}`}> {/* Add active class */}
       <div className="d-flex justify-content-between align-items-center navbar-left">
-        <a className={`sidemenu-opener ${showMenu ? "" : "hidden"}`} onClick={(e) => { toogleMenu(e); }}>
+        <a className={`sidemenu-opener ${showMenu ? "" : "hidden"}`} onMouseEnter={() => setTooltip("sideMenu")} onMouseLeave={() => setTooltip("")} onClick={(e) => { toogleMenu(e); }}>
           <GoSidebarCollapse style={{ fontSize: '23px' }} />
+          {tooltip === "Menu" && <span className="tooltip-text">open sidebar</span>}
         </a>
-        <a className={`sidemenu-opener`}>
-          <PiNotePencilFill style={{ fontSize: '23px' }} onClick={() => navigate("/home")}/>
+        <a className={`sidemenu-opener`} onMouseEnter={() => setTooltip("newChat")} onMouseLeave={() => setTooltip("")}>
+          <PiNotePencilFill style={{ fontSize: '23px' }} onClick={() => navigate("/home")} />
+          {tooltip === "Note" && <span className="tooltip-text">new chat</span>}
         </a>
         <Link className="navbar-brand" to="home">
-          <div className="d-flex flex-row justify-content-between">
+          {/* <div className="d-flex flex-row justify-content-between">
             <img className="mr-2" src="/assets/images/katalitik-logo-4.png" alt="user-avatar" />
             <MdKeyboardArrowDown style={{color:"#8E8E93", marginLeft:'6px'}}/>
           </div>
-          <div className="navbar-brand-text">1.5 Flash</div>
-          {/* <div>Kementrian Perhubungan<br/>Republik Indonesia</div> */}
+          <div className="navbar-brand-text">1.5 Flash</div> */}
+          <div className="d-flex flex-row justify-content-between" onClick={togglePopup}>
+            <img className="mr-2" src="/assets/images/katalitik-logo-4.png" alt="user-avatar" />
+            {isOpen ? (
+              <MdKeyboardArrowUp style={{ color: "#8E8E93", marginLeft: '6px' }} />
+            ) : (
+              <MdKeyboardArrowDown style={{ color: "#8E8E93", marginLeft: '6px' }} />
+            )}
+          </div>
+          <div className="navbar-brand-text">{selectedOption}</div>
         </Link>
+        {isOpen && (
+          <div className="popup-menu">
+            <div className="d-flex flex-column" onClick={() => handleOptionClick("1.5 Flash")}>
+              <div className="popup-title">1.5 Flash</div>
+              <div className="popup-item">For daily assistants</div>
+            </div>
+            <div className="d-flex flex-column" onClick={() => handleOptionClick("2.0 Daily Flash Experimental")}>
+              <div className="popup-title">2.0 Daily Flash Experimental</div>
+              <div className="popup-item"> For smartest model and more</div>
+            </div>
+            <div className="popup-item2" onClick={() => handleOptionClick("ChatAi Advanced")}>
+              <div className="d-flex justify-content-between align-items-center">
+                <img src="assets/images/katalitik-logo-2.png" className="sidebar-icon" />
+                <div>ChatAi Advanced</div>
+                <div className="upgrade-btn1">
+                  Upgrade
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       <div className="d-flex align-items-center navbar-right">
         {/* <a className="nav-link user-profile d-flex align-items-center" href="/#" onClick={handleProfileClick}>
@@ -105,7 +148,7 @@ const Navbar = ({ user, logout, toogleMenu, showMenu }) => {
         {/* <a className="nav-link logout" href="/#" onClick={(e) => handleLogout(e)}>
           <RiLogoutCircleRLine />
         </a> */}
-        <button className="sign-in"  onClick={(e) => handleLogout(e)}>
+        <button className="sign-in" onClick={(e) => handleLogout(e)}>
           Sign In
         </button>
       </div>
